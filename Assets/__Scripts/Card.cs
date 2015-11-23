a47dca20c90e3ac030703f699cf01d0e304c4da0
gorco <antcal01@ucm.es>
2015-11-23 16:16:21 +0100

    Created the deck

---
+++ Assets/__Scripts/Card.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Card : MonoBehaviour
+{
+	public string suit; // Suit of the Card (C,D,H, or S)
+	public int rank; // Rank of the Card (1-14)
+	public Color color = Color.black; // Color to tint pips
+	public string colS = "Black"; // or "Red". Name of the Color
+								  // This List holds all of the Decorator GameObjects
+	public List<GameObject> decoGOs = new List<GameObject>();
+	// This List holds all of the Pip GameObjects
+
+	public List<GameObject> pipGOs = new List<GameObject>();
+	public GameObject back; // The GameObject of the back of the card
+	public CardDefinition def; // Parsed from DeckXML.xml
+
+	public bool faceUp
+	{
+		get
+		{
+			return (!back.activeSelf);
+		}
+		set
+		{
+			back.SetActive(!value);
+		}
+	}
+}
+
+[System.Serializable]
+public class Decorator
+{
+	// This class stores information about each decorator or pip from DeckXML
+	public string type; // For card pips, type = "pip"
+	public Vector3 loc; // The location of the Sprite on the Card
+	public bool flip = false; // Whether to flip the Sprite vertically
+	public float scale = 1f; // The scale of the Sprite
+}
+[System.Serializable]
+public class CardDefinition
+{
+	// This class stores information for each rank of card
+	public string face; // Sprite to use for each face card
+	public int rank; // The rank (1-13) of this card
+	public List<Decorator> pips = new List<Decorator>(); // Pips used
+	// Because decorators (from the XML) are used the same way on every card in
+	// the deck, pips only stores information about the pips on numbered cards
+}
---
+++ Assets/__Scripts/Prospector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Prospector : MonoBehaviour {
+
+	static public Prospector S;
+	public Deck deck;
+	public TextAsset deckXML;
+
+	void Awake()
+	{
+		S = this; // Set up a Singleton for Prospector
+	}
+
+	void Start()
+	{
+		deck = GetComponent<Deck>(); // Get the Deck
+		deck.InitDeck(deckXML.text); // Pass DeckXML to it
+		Deck.Shuffle(ref deck.cards);    // This shuffles the deck
+		// The ref keyword passes a reference to deck.cards, which allows
+		//   deck.cards to be modified by Deck.Shuffle()
+	}
+}
